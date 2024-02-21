5e11180d103c34cad40f7f32b653105283cfe841
fifiuro <34020128+fifiuro@users.noreply.github.com>
2024-02-21 15:27:13 -0400

    Vue 3 app

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <img src="./assets/logo.svg" alt="Logo VUE" width="125" height="125" />
+    <small><h1>Hola mundo desde vuejs 3</h1></small>
+    <hr />
+
+    <h2>Contador: {{ contador }}</h2>
+    <button @click="incrementar()">+1</button>
+    <button @click="disminuir()">-1</button>
+    <button @click="reset()">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    contador: 10,
+  }),
+  methods: {
+    incrementar() {
+      this.contador++;
+    },
+    disminuir() {
+      this.contador--;
+    },
+    reset() {
+      this.contador = 10;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+
+small {
+  color: #666;
+}
+h1 {
+  font-weight: bolder;
+}
+</style>
